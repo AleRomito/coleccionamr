b134aae79dc4634e86bb4c7c1349921ded5d8f23
AleRomito <151970083+AleRomito@users.noreply.github.com>
2025-05-11 00:58:25 -0300

    Add files via upload

--- Assets/despliegueMenu.js
+++ Assets/despliegueMenu.js
@@ -22,32 +22,43 @@ document.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('scroll', () => {
     const navbar = document.getElementById('navBar1');
   
-    if (window.scrollY > 800) {
+    if (window.scrollY > 700) {
       navbar.classList.add('transparent');
     } else {
       navbar.classList.remove('transparent');
     }
   });
 
-  // document.getElementById("toggleBebidas").addEventListener("click", function () {
-  //   const bebidas = document.querySelector(".bebidas");
-  //   bebidas.classList.toggle("oculto");
-  // });
 
-  document.getElementById("toggleBebidas").addEventListener("click", function () {
+document.getElementById("toggleBebidas").addEventListener("click", function () {
     const bebidas = document.querySelector(".bebidas");
     const boton = document.getElementById("toggleBebidas");
+    const chevron = document.getElementById("chevronAnimado");
     const estabaOculto = bebidas.classList.contains("oculto2");
 
     bebidas.classList.toggle("oculto2");
 
     if (estabaOculto) {
-      // Ocultar el botón
-      boton.style.display = "none";
+        // Ocultar el botón y el chevron
+        boton.style.display = "none";
+        chevron.classList.add("ocultoChevron");
 
-      // Scroll hacia el div mostrado
-      setTimeout(() => {
-        bebidas.scrollIntoView({ behavior: "smooth", block: "start" });
-      }, 100);
+        // Scroll hacia el div mostrado
+        setTimeout(() => {
+            bebidas.scrollIntoView({ behavior: "smooth", block: "start" });
+        }, 100);
     }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.getElementById("navBar1");
+
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 50) {
+        navbar.classList.add("visible");
+      } else {
+        navbar.classList.remove("visible");
+        navbar.style.position = "fixed";
+      }
+    });
   });
